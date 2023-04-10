b18c88ca3fc6d8be9361ca4234b16796378eccf3
pathnirvana <pathnirvana@gmail.com>
2023-04-10 09:55:39 +0000

    misc

--- recipes/pathnirvana/download_pali.sh
+++ recipes/pathnirvana/download_pali.sh
@@ -3,12 +3,13 @@
 RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo $RUN_DIR
 # download Path Nirvana Sinhala dataset
-wget https://github.com/pnfo/pali-tts-dataset/releases/download/v1.0/pali_dataset.tar.bz2
+wget https://github.com/pnfo/pali-tts-dataset/releases/download/v1.1/pali_dataset.tar.bz2.partaa
+wget https://github.com/pnfo/pali-tts-dataset/releases/download/v1.1/pali_dataset.tar.bz2.partab
 # extract
 mkdir pali_dataset
-tar -xjf pali_dataset.tar.bz2 --directory pali_dataset
+cat pali_dataset.tar.bz2.part* | tar -xjf --directory pali_dataset
 
 shuf pali_dataset/metadata.csv > pali_dataset/metadata_shuf.csv
 
 mv pali_dataset $RUN_DIR/recipes/pathnirvana/
-rm pali_dataset.tar.bz2
+rm pali_dataset.tar.bz2.part*
