41d3c819e941fb22ac7986352b4494f3090aa09f
pathnirvana <pathnirvana@gmail.com>
2023-04-10 03:02:57 +0000

    support for sinhala letters

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -614,3 +614,16 @@ def pathnirvana(root_path, meta_file, **kwargs):  # pylint: disable=unused-argum
             speaker_name = cols[3]
             items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
     return items
+
+def pathnirvana2(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
+    """Path Nirvana Sinhala and Pali datasets from https://github.com/pnfo/"""
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("|")
+            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
+            text = cols[1] # take the sinhala text as the main text
+            speaker_name = cols[3]
+            items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
+    return items
